a2a40de25635fef601c944e5d60e66fda8a44992
justinette2175 <jugagnepain75@gmail.com>
2023-08-28 15:13:40 -0400

    update configs to try and fix admin bug

--- config/server.js
+++ config/server.js
@@ -1,10 +1,17 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
+  cron: { enabled: true },
+  cors: { enabled: true, origin: ["*"] },
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "9bf8cc74ab83590b280df0851beaec60"),
+    },
   },
 });
